d41f47a9c98df5ac9a83d11ed9d2fe74daf62fa6
laviniamarante <laviinia.amarante@gmail.com>
2026-05-11 21:06:18 -0300

    Primeiro commit

---
+++ app/home.tsx
@@ -0,0 +1,327 @@
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+
+export default function Perfil() {
+  return (
+
+    <><View style={styles.topNavbar}>
+
+      <TouchableOpacity>
+        <Entypo name="menu" size={32} color="white" />
+      </TouchableOpacity>
+
+      <View style={styles.logoArea}>
+        <Text style={styles.logoText}>GerencIF</Text>
+        <Text style={styles.logoSubtitle}>Gestão de Contratos</Text>
+      </View>
+
+    </View><View style={styles.screen}>
+
+        <ScrollView contentContainerStyle={styles.container}>
+
+          <Text style={styles.title}>
+            Contratos Recentes
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Últimos contratos cadastrados no sistema
+          </Text>
+
+          {/* card1 */}
+          <View style={styles.card}>
+
+            <View style={styles.topCard}>
+
+              <Text style={styles.cardText}>
+                Serviços de Limpeza e Conservação
+              </Text>
+
+              <View style={styles.statusAtivo}>
+                <Text style={styles.statusText}>Ativo</Text>
+              </View>
+
+            </View>
+
+            <Text style={styles.cardSubtitle}>
+              Fornecedor: Limpeza Total Serviços
+            </Text>
+
+            <Text style={styles.cardSubtitle}>
+              Vencimento: 12/07/2026
+            </Text>
+
+            <TouchableOpacity style={styles.detalhes}>
+              <Text style={styles.detalhesText}>
+                Ver detalhes
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+
+          {/* CARD 2 */}
+          <View style={styles.card}>
+
+            <View style={styles.topCard}>
+
+              <Text style={styles.cardText}>
+                Fornecimento de materiais de escritório
+              </Text>
+
+              <View style={styles.statusVencido}>
+                <Text style={styles.statusText}>Vencido</Text>
+              </View>
+
+            </View>
+
+            <Text style={styles.cardSubtitle}>
+              Fornecedor: Papelaria Central Ltda
+            </Text>
+
+            <Text style={styles.cardSubtitle}>
+              Vencimento: 15/08/2026
+            </Text>
+
+            <TouchableOpacity style={styles.detalhes}>
+              <Text style={styles.detalhesText}>
+                Ver detalhes
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+
+          {/* CARD 3 */}
+          <View style={styles.card}>
+
+            <View style={styles.topCard}>
+
+              <Text style={styles.cardText}>
+                Fornecimento de Equipamentos de Laboratório
+              </Text>
+
+              <View style={styles.statusAtivo}>
+                <Text style={styles.statusText}>Ativo</Text>
+              </View>
+
+            </View>
+
+            <Text style={styles.cardSubtitle}>
+              Fornecedor: Laboratórios Avançados S.A.
+            </Text>
+
+            <Text style={styles.cardSubtitle}>
+              Vencimento: 20/09/2026
+            </Text>
+
+            <TouchableOpacity style={styles.detalhes}>
+              <Text style={styles.detalhesText}>
+                Ver detalhes
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+
+          {/* CARD 4 */}
+          <View style={styles.card}>
+
+            <View style={styles.topCard}>
+
+              <Text style={styles.cardText}>
+                Serviço de Alimentação
+              </Text>
+
+              <View style={styles.statusPendente}>
+                <Text style={styles.statusText}>Pendente</Text>
+              </View>
+
+            </View>
+
+            <Text style={styles.cardSubtitle}>
+              Fornecedor: Vale Verde Alimentação
+            </Text>
+
+            <Text style={styles.cardSubtitle}>
+              Vencimento: 20/11/2026
+            </Text>
+
+            <TouchableOpacity style={styles.detalhes}>
+              <Text style={styles.detalhesText}>
+                Ver detalhes
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+
+        </ScrollView>
+
+        <View style={styles.navbar}>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Entypo name="home" size={30} color="#006C5B" />
+            <Text style={styles.navText}>Home</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Entypo name="folder" size={30} color="#006C5B" />
+            <Text style={styles.navText}>Contratos</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Entypo name="dots-three-horizontal" size={30} color="#006C5B" />
+            <Text style={styles.navText}>Mais</Text>
+          </TouchableOpacity>
+
+        </View>
+
+      </View></>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  topNavbar: {
+  backgroundColor: '#006C5B',
+  width: '100%',
+
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+
+  paddingHorizontal: 25,
+  paddingTop: 30,
+  paddingBottom: 25,
+},
+
+logoArea: {
+  marginLeft: 20,
+},
+
+logoText: {
+  color: 'white',
+  fontSize: 25,
+  fontWeight: 'bold',
+},
+
+logoSubtitle: {
+  color: 'white',
+  fontSize: 16,
+},
+
+  navbar: {
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+
+  backgroundColor: '#d9e7e2',
+  paddingVertical:20
+},
+
+navItem: {
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+
+navText: {
+  fontSize: 12,
+  color: '#006C5B',
+},
+
+screen: {
+  flex: 1,
+},
+
+  container: {
+    paddingBottom: 120,
+    flexGrow: 1,
+    padding: 20,
+    paddingTop: 20,
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+    marginBottom: 30,
+  },
+
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    height: 200,
+    marginBottom: 15,
+  },
+
+  topCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  cardText: {
+    fontSize: 16,
+    width: '75%',
+  },
+
+  cardSubtitle: {
+    fontSize: 16,
+    color: 'gray',
+    marginTop: 7,
+  },
+
+  statusAtivo: {
+    backgroundColor: '#39c172',
+    paddingVertical: 5,
+    paddingHorizontal: 18,
+    borderRadius: 5,
+    marginTop: -10,
+  },
+
+  statusVencido: {
+    backgroundColor: '#e53935',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    marginTop: -10,
+  },
+
+  statusPendente: {
+    backgroundColor: '#f5ba18',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    marginTop: -10, 
+  },
+
+  statusText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+
+  detalhes: {
+    backgroundColor: '#add8d1',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    marginTop: 'auto',
+    marginBottom: -10,
+  },
+
+  detalhesText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#006C5B',
+    textAlign: 'center',
+  },
+
+});
---
+++ app/login.tsx
@@ -0,0 +1,142 @@
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { router } from "expo-router";
+import Fontisto from '@expo/vector-icons/Fontisto';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        
+        <Text style={styles.title}>GerencIF</Text>
+        <Text style={styles.subtitle}>
+          Sistema de Gerenciamento de Contratos do IFPB Campus Esperança
+        </Text>
+
+     <Text style={styles.label}>Email</Text>
+     <View style={styles.inputContainer}>
+
+  <Fontisto name="email" size={25} color="#006C5B" />
+
+  <TextInput
+    style={styles.input}
+    placeholder="Digite seu email..."
+    value={email}
+    onChangeText={setEmail}
+  />
+
+</View>
+
+       <Text style={styles.label}>Senha</Text>
+
+<View style={styles.inputContainer}>
+
+  <AntDesign name="lock" size={25} color="#006C5B" />
+
+  <TextInput
+    style={styles.input}
+    placeholder="Digite sua senha..."
+    secureTextEntry={true}
+    value={senha}
+    onChangeText={setSenha}
+  />
+
+</View>
+
+        <View style={styles.row}>
+          <Text>Lembrar-me</Text>
+          <Text style={styles.link}>Esqueci minha senha</Text>
+        </View>
+
+       <TouchableOpacity
+       style={styles.button}
+       onPress={() => router.replace("/home")}
+       >
+       <Text style={styles.buttonText}>Entrar</Text>
+       </TouchableOpacity>
+
+        <Text style={styles.footer}>
+          Não tem uma conta? <Text style={styles.link}>Crie uma conta</Text>
+        </Text>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: '85%',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    borderWidth: 0.7,
+    borderColor: '#bebcbc',
+    height: '80%',
+    padding: 30
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#006C5B',
+    textAlign: 'center',
+    marginTop: 60
+  },
+
+  subtitle: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 50
+  },
+  label: {
+    marginTop: 25,
+    fontWeight: '600',
+  },
+input: {
+  flex: 1,
+  padding: 12,
+},
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  link: {
+    color: '#006C5B',
+  },
+  button: {
+    backgroundColor: '#006C5B',
+    padding: 15,
+    borderRadius: 8,
+    marginTop: 70,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  footer: {
+    textAlign: 'center',
+    marginTop: 15,
+  },
+ inputContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+
+  borderWidth: 1,
+  borderColor: '#ccc',
+  borderRadius: 8,
+
+  paddingHorizontal: 10,
+  marginTop: 5,
+},
+});
