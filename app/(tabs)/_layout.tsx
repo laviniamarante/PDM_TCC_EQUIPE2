b0518cc360d8c09e3c2be412349e5e9fd8231c09
laviniamarante <laviinia.amarante@gmail.com>
2026-05-26 11:29:33 -0300

    Ajeitei o tab e comecei a tela de detalhes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,41 @@
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+    screenOptions={{
+    headerShown: false,
+    tabBarActiveTintColor: '#006C5B',
+    tabBarInactiveTintColor: '#575656',
+    tabBarStyle: {
+      backgroundColor: '#d7e4e2',
+      height: 80,
+      paddingTop: 10,
+    },
 
+    
+    }}
+    >
       <Tabs.Screen
         name="home"
-        options={{ title: "Home" }}
+        options={{ title: "Home",
+          tabBarIcon: () => (
+            <FontAwesome name="home" size={25} color={'#006C5B'} />
+          ),
+         }}
       />
 
       <Tabs.Screen
-        name="contratos"
-        options={{ title: "Contratos" }}
-      />
-
-      <Tabs.Screen
-        name="mais"
-        options={{ title: "Lembretes" }}
-      />
+         name="lembretes"
+         options={{
+         title: "Lembretes",
+         tabBarIcon: () => (
+      <Ionicons name="notifications" size={25} color={'#006C5B'} />
+    ),
+  }}
+/>
 
     </Tabs>
   );
--- app/detalhesContrato.tsx
+++ app/detalhesContrato.tsx
@@ -1,13 +1,85 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
 
 export default function DetalhesContrato() {
 
-  const { id } = useLocalSearchParams();
-
   return (
-    <View>
-        <Text> Detalhes do Contrato</Text>
+    <View style={styles.container}>
+      <View style={styles.topBar} />
+
+      <View style={styles.header}>
+
+        <View style={styles.headerRow}>
+          <TouchableOpacity style={styles.backButton}>
+            <Feather name="arrow-left" size={24} color="black" />
+          </TouchableOpacity>
+
+          <View>
+            <Text style={styles.titulo}>
+              Detalhes do contrato
+            </Text>
+
+            <Text style={styles.subtitulo}>
+              Visualize todas as informações do contrato
+            </Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View style={styles.card} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F3F4',
+  },
+
+  topBar: {
+    backgroundColor: '#006C5B',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 25,
+    paddingTop: 60,
+    paddingBottom: 25,
+  },
+
+  header: {
+    padding: 20,
+  },
+
+  headerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  backButton: {
+    marginRight: 12,
+  },
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+
+  subtitulo: {
+    color: 'gray',
+    fontSize: 15,
+  },
+
+  card: {
+    backgroundColor: 'white',
+    marginHorizontal: 18,
+    borderRadius: 10,
+    marginTop: 10,
+    minHeight: 250,
+    height: '70%',
+  },
+});
